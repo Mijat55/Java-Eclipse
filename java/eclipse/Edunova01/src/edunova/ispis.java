6557f7f0a50a566f981b3437727cd2c5026faf6f
Marko Mijatović <mijat5555@gmail.com>
2022-12-15 17:42:22 +0100

    Hello

---
+++ java/eclipse/Edunova01/src/edunova/ispis.java
@@ -0,0 +1,22 @@
+package edunova;
+
+public class ispis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Prva Linija");
+		System.out.println("Druga Linija");
+		
+		System.out.print("Prva\nDruga\n");
+		// ako nije ln u syso onda ce pisati sve u jedan red moramo dodati \n to jest novi red
+		
+		System.out.println("Osijek");
+		// \t znaci razmak 
+		
+//	
+		
+
+	}
+
+}
