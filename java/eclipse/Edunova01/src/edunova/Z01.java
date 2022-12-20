a1b4e41782747adf0534a906a1e9f21fb65e4de9
Marko Mijatović <mijat5555@gmail.com>
2022-12-20 14:54:33 +0100

    Primitivni tipovi 8

---
+++ java/eclipse/Edunova01/src/edunova/Z01.java
@@ -0,0 +1,17 @@
+package edunova;
+
+public class Z01 {
+	public static void main(String[] args) {
+		int a= 7; // cijeli
+		boolean aktivan = true ; // da ne 
+		short mali = 129; // cijeli manji
+		byte maliBroj = 12; //najmanji
+		double srednji = 2020; //veci decimal
+		char jedan = 1; //znak samo jedan 
+		long veci = 20202; //cijeli veci
+		float decimala = 3.14f; //decimalni
+		
+		
+	}
+
+}
