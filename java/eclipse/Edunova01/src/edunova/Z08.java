89375ae65d95d9b743356b55edc09816baaa664d
Marko Mijatović <mijat5555@gmail.com>
2022-12-23 15:22:57 +0100

    Z08

---
+++ java/eclipse/Edunova01/src/edunova/Z08.java
@@ -0,0 +1,25 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z08 {
+	public static void main(String[] args) {
+		 // Korisnik unosi dva cijela broja
+		 // Program ispisuje 
+		 // zbroj svih parnih brojeva
+		 // između dva unesena broja
+		
+		int prviBroj= Integer.parseInt(JOptionPane.showInputDialog("Unos prvog broja"));
+		int drugiBroj=Integer.parseInt(JOptionPane.showInputDialog("Unos drugog broja"));
+		int min = prviBroj<drugiBroj ? prviBroj : drugiBroj;
+		int max = prviBroj>drugiBroj ? prviBroj : drugiBroj;
+		int zbroj=0;
+		for(int i=min;i<=max;i++) {
+			if(i%2==0) {
+				zbroj+=i;
+			}
+		}
+		System.out.println(zbroj);
+	}
+
+}
