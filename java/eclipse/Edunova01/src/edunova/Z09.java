f0a748245ba707cf25351b871eda39ed789208ac
Marko Mijatović <mijat5555@gmail.com>
2022-12-23 16:06:18 +0100

    vjezba

---
+++ java/eclipse/Edunova01/src/edunova/Z09.java
@@ -0,0 +1,14 @@
+package edunova;
+
+public class Z09 {
+	public static void main(String[] args) {
+		// Program ispisuje brojeve od 100 do 1
+		// u istom redu odvojeno zarezom
+		
+		 System.out.println("Ispis brojeva od 1 do 100 u istoj liniji");
+		    for (int i=1; i<= 100; i++)        {
+		         System.out.print(i + ",");
+		    }
+	}
+
+}
