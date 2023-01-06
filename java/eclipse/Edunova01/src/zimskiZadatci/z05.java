d6b296df0f4ca314a52d522ac8d085513acd1bcd
Marko Mijatović <mijat5555@gmail.com>
2023-01-06 14:23:24 +0100

    TABLICA MNOZENJA 15X15

---
+++ java/eclipse/Edunova01/src/zimskiZadatci/z05.java
@@ -0,0 +1,15 @@
+package zimskiZadatci;
+
+public class z05 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 15; i++) {
+
+			for (int j = 1; j <= 15; j++) {
+				System.out.print(i * j + " ");
+			}
+
+			System.out.println();
+		}
+	}
+}
