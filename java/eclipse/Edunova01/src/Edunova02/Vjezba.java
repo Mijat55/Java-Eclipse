3fca6e7026add516e533ae4380f89888b9da8c58
Marko Mijatović <mijat5555@gmail.com>
2023-01-10 12:42:07 +0100

    Vjezbe

---
+++ java/eclipse/Edunova01/src/Edunova02/Vjezba.java
@@ -0,0 +1,19 @@
+package Edunova02;
+
+public class Vjezba {
+	public static void main(String[] args) {
+		int zbroj=0;
+		for (int i=0;i<10;i++) {
+			if(i>2 && i<7) {
+				System.out.println("Marko");
+			}else
+			{System.out.println(i);
+			
+			
+			
+			
+		}
+	}
+
+	}
+}
