797e32083bbb8f01d06b35002ad4ece62180848c
Marko Mijatović <mijat5555@gmail.com>
2023-01-11 13:51:32 +0100

    vjezba IF-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z06.java
@@ -0,0 +1,21 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z06 {
+	public static void main(String[] args) {
+		// Za dva unesena cijela broja
+		// program ispisuje Osijek
+		// ako je zbroj veći od 10
+		// inače ispisuje Edunova
+		
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int j = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		if(i+j>10) {
+			System.out.println("Osijek");
+		}else
+		{System.out.println("Edunova");
+	}
+
+}
+}
