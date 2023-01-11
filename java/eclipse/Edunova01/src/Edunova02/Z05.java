42e647800e56dc2c91f4542225ce7befc0b17ef2
Marko Mijatović <mijat5555@gmail.com>
2023-01-11 13:47:35 +0100

    vjezba IF-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z05.java
@@ -0,0 +1,19 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z05 {
+	public static void main(String[] args) {
+		// Korisnik unosi cijeli broj
+		// Program ispisuje da li je 
+		// paran ili nije
+		
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj"));
+		if(i%2==0) {
+			System.out.println("Paran");
+		}else
+		{System.out.println("Neparan");
+	}
+
+}
+}
