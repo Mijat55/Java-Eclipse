aa0c2f2c16f8d05eeefa2f3e87cdbb8a1c42e417
Marko Mijatović <mijat5555@gmail.com>
2023-01-11 14:32:35 +0100

    vjezba IF-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z08.java
@@ -0,0 +1,24 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z08 {
+	public static void main(String[] args) {
+		// Za upisana 3 cijela broja
+		// program ispisuje najveći
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int j = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int k = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+
+		if (i >= j && i >= k) {
+			System.out.println(i);
+
+		} else if (j >= k && j >= i) {
+			System.out.println(j);
+		} else if (k >= j && k >= i) {
+			System.out.println(k);
+		}
+
+	}
+
+}
