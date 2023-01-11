201abd5a1e220a0b077b84592fccbba0925dbe7e
Marko Mijatović <mijat5555@gmail.com>
2023-01-11 14:50:48 +0100

    vjezba IF_a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z09.java
@@ -0,0 +1,36 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z09 {
+	public static void main(String[] args) {
+		// program unosi broj između 
+		// 1 i 999
+		// U slučaju da je izvan tog raspona
+		// ispisati grešku i prekinuti program
+		// Program ispisuje 1. znamenku upisanog broja
+		int i =Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		
+		if(i>999 ) {
+			System.out.println("Greska"); 
+			
+		}else System.out.println(i);
+		
+	
+			
+		
+			
+			
+			
+			
+		}
+		
+		
+			
+	
+		
+		
+	}
+
+
+
