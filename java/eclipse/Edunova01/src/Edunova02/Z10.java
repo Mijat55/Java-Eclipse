9cdf0eacfc97bdf004e4eac343303f17921c49d2
Marko Mijatović <mijat5555@gmail.com>
2023-01-12 14:47:08 +0100

    vjezba IF-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z10.java
@@ -0,0 +1,28 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z10 {
+	public static void main(String[] args) {
+		// Program unosi 2 broja
+		// Ako su oba broja parni
+		// ispisuje njihov zbroj
+		// inače ispisuje njihovu razliku
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int j = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		boolean zbroj=true;
+		if(i%2==0 && j%2==0) {
+			zbroj=true;
+				System.out.println(i+j);
+			}else 
+				{zbroj=false; 
+				System.out.println(i-j);
+				
+			
+				
+			}
+		
+		
+	}
+
+}
