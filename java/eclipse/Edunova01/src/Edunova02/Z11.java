c233c33c1d6c1cc3182d1b60ad95d3c3b6f6c8e4
Marko Mijatović <mijat5555@gmail.com>
2023-01-12 15:06:07 +0100

    vjezba For-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z11.java
@@ -0,0 +1,14 @@
+package Edunova02;
+
+public class Z11 {
+	public static void main(String[] args) {
+		// Program ispisuje brojeve od 100 do 1
+		// u istom redu odvojeno zarezom
+		
+		for(int i=100;i>0;i--) 
+			
+		
+		System.out.print(i + ",");
+	}
+
+}
