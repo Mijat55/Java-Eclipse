8c16c1ea6fa6f2b19131e05780884ba2b9743799
Marko Mijatović <mijat5555@gmail.com>
2023-01-12 15:28:31 +0100

    vjezba For-a

---
+++ java/eclipse/Edunova01/src/Edunova02/Z12.java
@@ -0,0 +1,28 @@
+package Edunova02;
+
+public class Z12 {
+	public static void main(String[] args) {
+		//ispisati sve parne brojeve od 12 do 35
+		//ispisati svaki 5 broj od 5 do 100
+		for(int i=11;i<35;i++) {
+			if(i%2==0) {
+				System.out.print(i+",");
+			}
+		}
+		System.out.println();
+		
+		for(int i=5;i<=50;i+=5) {
+			
+			System.out.println(i);
+			
+			 
+				
+			
+			}
+			
+		
+			
+		}
+	}
+
+
