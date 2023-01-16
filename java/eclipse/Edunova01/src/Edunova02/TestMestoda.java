9d27b0f1555b02e51d90bb18c5878189f1c076fd
Marko Mijatović <mijat5555@gmail.com>
2023-01-16 14:39:59 +0100

    VjezbaMetode

--- java/eclipse/Edunova01/src/Edunova02/TestMestoda.java
+++ java/eclipse/Edunova01/src/Edunova02/TestMestoda.java
@@ -3,7 +3,11 @@ package Edunova02;
 public class TestMestoda {
 	public static void main(String[] args) {
 		System.out.println(Z15.najveci(5, 8, 2));
+		
+		System.out.println(Z16.broj());
 	}
+	
+	
 
 	
 
---
+++ java/eclipse/Edunova01/src/Edunova02/Z16.java
@@ -0,0 +1,19 @@
+package Edunova02;
+
+import javax.swing.JOptionPane;
+
+public class Z16 {
+	// Metoda od korisnika traži
+		// unos broja između 1 i 10
+		// te vraća ispravno unesen broj
+	public static int broj() {
+		int i;
+		while(true) {
+			i=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj izmedju 1 i 10"));
+		if(i>=1 && i<=10) {
+			return i;
+		}
+	}
+
+}
+}
