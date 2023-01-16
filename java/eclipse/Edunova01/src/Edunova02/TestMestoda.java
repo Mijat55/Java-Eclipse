85f7c7cdbd64da1912c584eae40bf4d5117352bc
Marko Mijatović <mijat5555@gmail.com>
2023-01-16 14:31:36 +0100

    VjezbaMetode

---
+++ java/eclipse/Edunova01/src/Edunova02/TestMestoda.java
@@ -0,0 +1,12 @@
+package Edunova02;
+
+public class TestMestoda {
+	public static void main(String[] args) {
+		System.out.println(Z15.najveci(5, 8, 2));
+	}
+
+	
+
+	}
+
+
---
+++ java/eclipse/Edunova01/src/Edunova02/Z15.java
@@ -0,0 +1,16 @@
+package Edunova02;
+
+public class Z15 {
+	// Napišite metodu koja prima
+		// tri cijela broja i vraća najveći
+	public static int najveci(int a, int b, int c) {
+		if(a>=b && a>=c) {
+			return a;
+		}
+		if(b>=a && b>=c) {
+			return b;
+		}
+		return c;
+	}
+
+}
