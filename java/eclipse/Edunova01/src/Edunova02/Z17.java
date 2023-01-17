d27006cafa2bc1ab339e5a05563b95ae3a39cd81
Marko Mijatović <mijat5555@gmail.com>
2023-01-17 18:35:28 +0100

    Vjezba_Metoda

--- java/eclipse/Edunova01/src/Edunova02/Z17.java
+++ java/eclipse/Edunova01/src/Edunova02/Z17.java
@@ -2,4 +2,20 @@ package Edunova02;
 
 public class Z17 {
 
+	// Program od korisnika traži ukupan broj
+	// predmeta u školi
+	// Program unosi ocjene za svaki predmet
+	// i ispisuje prosjek ocjena
+
+	public static void main(String[] args) {
+		int suma = 0;
+
+		int ukupnoPredmeta = Test.ucitajCijeliPozitivniBroj("Unesite ukupan broj predmeta za koji želite prosjek");
+		for (int i = 0; i < ukupnoPredmeta; i++) {
+
+			suma += Test.ucitajCijeliBrojURasponu("Unesite " + (i + 1) + ". ocjenu", 1, 5);
+		}
+
+		System.out.println(suma / (float) ukupnoPredmeta);
+	}
 }
