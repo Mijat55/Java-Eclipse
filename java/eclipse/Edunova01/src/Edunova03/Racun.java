b1ce0c09dd1ca35447cd7d5663a74d6f93ef15df
Marko Mijatović <mijat5555@gmail.com>
2023-01-23 15:28:13 +0100

    Zadatak2

---
+++ java/eclipse/Edunova01/src/Edunova03/Racun.java
@@ -0,0 +1,29 @@
+package Edunova03;
+
+import java.util.Date;
+
+public class Racun {
+	private Date date;
+	private double iznos;
+	private Kupac kupac;
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public double getIznos() {
+		return iznos;
+	}
+	public void setIznos(double iznos) {
+		this.iznos = iznos;
+	}
+	public Kupac getKupac() {
+		return kupac;
+	}
+	public void setKupac(Kupac kupac) {
+		this.kupac = kupac;
+	}
+	
+
+}
---
+++ java/eclipse/Edunova01/src/Edunova03/Racunalo.java
@@ -0,0 +1,41 @@
+package Edunova03;
+
+public class Racunalo {
+	private String imeModela;
+	private int cijenaUredjaja;
+	private double brzinaProcesora;
+	private boolean azuriranje;
+	private char energetskaVrijednost;
+	public String getImeModela() {
+		return imeModela;
+	}
+	public void setImeModela(String imeModela) {
+		this.imeModela = imeModela;
+	}
+	public int getCijenaUredjaja() {
+		return cijenaUredjaja;
+	}
+	public void setCijenaUredjaja(int cijenaUredjaja) {
+		this.cijenaUredjaja = cijenaUredjaja;
+	}
+	public double getBrzinaProcesora() {
+		return brzinaProcesora;
+	}
+	public void setBrzinaProcesora(double brzinaProcesora) {
+		this.brzinaProcesora = brzinaProcesora;
+	}
+	public boolean isAzuriranje() {
+		return azuriranje;
+	}
+	public void setAzuriranje(boolean azuriranje) {
+		this.azuriranje = azuriranje;
+	}
+	public char getEnergetskaVrijednost() {
+		return energetskaVrijednost;
+	}
+	public void setEnergetskaVrijednost(char energetskaVrijednost) {
+		this.energetskaVrijednost = energetskaVrijednost;
+	}
+	
+
+}
