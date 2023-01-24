211caa9493e468ee6e20dac3f13c45e7595bd730
Marko Mijatović <mijat5555@gmail.com>
2023-01-24 16:31:28 +0100

    Multiples of 3 or 5

---
+++ java/eclipse/Edunova01/src/projectEuler/multiples_of_3_or_5.java
@@ -0,0 +1,36 @@
+package projectEuler;
+
+public class multiples_of_3_or_5 {
+	public static void main(String[] args) {
+		int zbroj=0;
+		for(int i=0;i<=1000;i++) { 
+			if(i%3==0) {
+				System.out.println(i);
+			}else if  (i%5==0) {
+				System.out.println(i);
+			}
+			
+				zbroj+=i;
+				
+		}
+		System.out.println(zbroj);
+			
+			
+			
+		 
+		
+		
+		
+		
+		
+			}
+		}
+	
+
+			
+		
+		
+		
+
+
+
